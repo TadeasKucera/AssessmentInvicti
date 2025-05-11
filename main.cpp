5c39305eebe7b2e5893c8711bae2c422f3c18741
Tadeas Kucera <tadeas.kucera@seznam.cz>
2025-05-11 11:28:24 +0200

    Send request to userinfo.php instead of login.php

--- main.cpp
+++ main.cpp
@@ -6,16 +6,15 @@
 
 int main() {
     cpr::Session session;
-    session.SetUrl(cpr::Url{ "http://testphp.vulnweb.com/login.php" });
+    session.SetUrl(cpr::Url{ "http://testphp.vulnweb.com/userinfo.php" });
     session.SetPayload({ {"uname", "test"}, {"pass", "test"} });
-    session.SetRedirect(cpr::Redirect{ true });
 
-    cpr::Response r = session.Post();
+    const auto r = session.Post();
 
     std::cout << "Status Code: " << r.status_code << std::endl;
 
-    std::string filename = "response.html";
-    std::ofstream file(filename);
+    const std::string filename{ "response.html" };
+    std::ofstream file{ filename };
     file << r.text;
     file.close();
 
