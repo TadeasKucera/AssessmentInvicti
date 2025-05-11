0ea06384d60d7c31a2235b22bdb651760df663d2
Tadeas Kucera <tadeas.kucera@seznam.cz>
2025-05-11 11:35:56 +0200

    Handle the system call for other platforms.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,27 @@ int main() {
     session.SetUrl(cpr::Url{ "http://testphp.vulnweb.com/userinfo.php" });
     session.SetPayload({ {"uname", "test"}, {"pass", "test"} });
 
+    // Post HTML request
     const auto r = session.Post();
 
+    // Print to the console
     std::cout << "Status Code: " << r.status_code << std::endl;
+    std::cout << "Response text: " << r.text.substr(100) << std::endl;
 
+    // Write the HTML to a file
     const std::string filename{ "response.html" };
     std::ofstream file{ filename };
     file << r.text;
     file.close();
 
+    // Open with default browser
+#ifdef _WIN32
     system(("start " + filename).c_str());
+#elif __APPLE__
+    system(("open " + filename).c_str());
+#elif __linux__
+    system(("xdg-open " + filename).c_str());
+#endif
 
     return 0;
 }
