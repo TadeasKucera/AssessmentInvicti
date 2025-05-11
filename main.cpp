a16e9c1aac9db1ba9ec5dfd55cf8a831370e282c
Tadeas Kucera <tadeas.kucera@seznam.cz>
2025-05-11 12:00:23 +0200

    Put the whole logic in main.cpp into a try block.

--- main.cpp
+++ main.cpp
@@ -9,28 +9,37 @@ consteval const char* post_url() {
 }
 
 int main() {
-    cpr::Session session;
-    session.SetUrl(cpr::Url{ post_url()});
-    session.SetPayload({ {"uname", "test"}, {"pass", "test"} });
+    try {
+        cpr::Session session;
+        session.SetUrl(cpr::Url{ post_url() });
+        session.SetPayload({ {"uname", "test"}, {"pass", "test"} });
 
-    // Post HTML request
-    const auto r = session.Post();
+        // Post HTML request
+        const auto r = session.Post();
 
-    // Print to the console
-    std::cout << "Status Code: " << r.status_code << std::endl;
+        // Print to the console
+        std::cout << "Status Code: " << r.status_code << std::endl;
 
-    // Write the HTML to a file
-    const std::string filename{ "response.html" };
-    std::ofstream file{ filename };
-    file << r.text;
-    file.close();
+        // Write the HTML to a file
+        const std::string filename{ "response.html" };
+        std::ofstream file{ filename };
+        file << r.text;
+        file.close();
 
-    // Open with default browser
+        // Open with default browser
 #ifdef _WIN32
-    system(("start " + filename).c_str());
+        system(("start " + filename).c_str());
 #elif __APPLE__
-    system(("open " + filename).c_str());
+        system(("open " + filename).c_str());
 #elif __linux__
-    system(("xdg-open " + filename).c_str());
+        system(("xdg-open " + filename).c_str());
 #endif
+    } catch (const std::exception& e) {
+        std::cerr << "Exception thrown: " << e.what() << std::endl;
+        return 1;
+    } catch (...) {
+        std::cerr << "Unknown exception occured." << std::endl;
+        return 2;
+    }
+    return 0;
 }
