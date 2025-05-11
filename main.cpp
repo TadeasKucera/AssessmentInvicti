dfefb8771c34495fb07d164e94827f3394a60b5b
Tadeas Kucera <tadeas.kucera@seznam.cz>
2025-05-11 12:18:15 +0200

    Move functionality outside main function

--- main.cpp
+++ main.cpp
@@ -8,32 +8,44 @@ consteval const char* post_url() {
     return "http://testphp.vulnweb.com/userinfo.php";
 }
 
+// Post HTML request to login to testphp.vulnweb.com
+cpr::Response post_request() {
+    cpr::Session session;
+    session.SetUrl(cpr::Url{ post_url() });
+    session.SetPayload({ {"uname", "test"}, {"pass", "test"} });
+    return session.Post();
+}
+
+// Write the HTML to a file
+void save_response_to_file(const std::string& filename, const std::string& content) {
+    std::ofstream file{ filename };
+    if (!file)
+    {
+        throw std::ios_base::failure("Failed to open file: " + filename);
+    }
+    file << content;
+    file.close();
+}
+
+// Open file with default application
+void open_in_browser(const std::string& filename) {
+#ifdef _WIN32
+    system(("start " + filename).c_str());
+#elif __APPLE__
+    system(("open " + filename).c_str());
+#elif __linux__
+    system(("xdg-open " + filename).c_str());
+#endif
+}
+
 int main() {
     try {
-        cpr::Session session;
-        session.SetUrl(cpr::Url{ post_url() });
-        session.SetPayload({ {"uname", "test"}, {"pass", "test"} });
-
-        // Post HTML request
-        const auto r = session.Post();
-
-        // Print to the console
+        const auto r = post_request();
         std::cout << "Status Code: " << r.status_code << std::endl;
 
-        // Write the HTML to a file
         const std::string filename{ "response.html" };
-        std::ofstream file{ filename };
-        file << r.text;
-        file.close();
-
-        // Open with default browser
-#ifdef _WIN32
-        system(("start " + filename).c_str());
-#elif __APPLE__
-        system(("open " + filename).c_str());
-#elif __linux__
-        system(("xdg-open " + filename).c_str());
-#endif
+        save_response_to_file(filename, r.text);
+        open_in_browser(filename);
     } catch (const std::exception& e) {
         std::cerr << "Exception thrown: " << e.what() << std::endl;
         return 1;
